dfe6c870da49ed1690804086e00c60effdcd3621
FacundoDayne <operavgxgamer@gmail.com>
2025-03-30 19:34:28 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,2 @@
-import os
-import time
-
-file = b"C:\Users\Vaynes\Documents\SEGA\PHANTASYSTARONLINE2_NA_STEAM\log_ngs\ChatLog20250329_00.txt"
-
-def _observer_thread_func() -> None:
-    last_size = os.path.getsize(file)
-
-    time.sleep(1)  
-    current_size = os.path.getsize(file)
-    if current_size > last_size:  
-        with open(file, "r", encoding="utf-16") as f:
-            f.seek(last_size) 
-            new_lines = f.readlines()
-            print(new_lines)
-
-        last_size = current_size  
-
-print("Watching")
-while True:
-    _observer_thread_func()
+def string_formatted(message: str) -> str:
+    
